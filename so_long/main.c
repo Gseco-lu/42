0fb603e78a71469fe1985b48598d4f092f433533
Gseco-lu <goncalosseco@gmail.com>
2022-06-03 17:47:03 +0100

    so_long

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: gseco-lu <gseco-lu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 17:36:36 by gseco-lu          #+#    #+#             */
-/*   Updated: 2022/06/01 20:27:38 by gseco-lu         ###   ########.fr       */
+/*   Updated: 2022/06/02 16:51:49 by gseco-lu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,19 +36,23 @@ int	main(void)
 	int		i;
 	int		raio;
 	int		j;
+	int		g;
 
-	i = 50;
-	j = 50;
+	i = 500;
+	j = 500;
+	g = 0;
 	raio = 50;
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "so_long");
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	while (i != 150)
-	{		
-		my_mlx_pixel_put(&img, i * 3.14, (j * 3.14), 0x00FF0000);
-		i++;
-		j++;
+	while (g != 200)
+	{
+		my_mlx_pixel_put(&img, i - g, j + g, 0x000000FF);
+		my_mlx_pixel_put(&img, i + g, j + g, 0x000000FF);
+		my_mlx_pixel_put(&img, i + g, j + 199, 0x000000FF);
+		my_mlx_pixel_put(&img, i - g, j + 199, 0x000000FF);
+		g++;
 	}
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);

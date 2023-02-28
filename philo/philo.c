03d1b0fc9a0e9f45af7ad9703e37425027aac8f8
Gseco-lu <94009977+Gseco-lu@users.noreply.github.com>
2023-02-28 01:16:32 +0000

    Add files via upload

---
+++ philo/philo.c
@@ -0,0 +1,95 @@
+#include "philo.h"
+
+void	ft_initms(t_philo *philo)
+{
+	struct timeval	t;
+
+	gettimeofday(&t, NULL);
+	philo->args->init_ms = (t.tv_sec * 1000) + (t.tv_usec / 1000);
+}
+
+int	init_philo(t_global *g)
+{
+	int	i;
+
+	i = -1;
+	while (++i < g->args->philo_nbr)
+	{
+		g->philo[i].arr = g->arr;
+		g->philo[i].arr_mtx = g->arrmtx;
+		g->philo[i].death_mtx = g->deathmtx;
+		g->philo[i].printmtx = g->print;
+		g->philo[i].mtx = g->mutexes;
+		g->philo[i].args = g->args;
+		g->philo[i].id = i + 1;
+		g->philo[i].left = i;
+		g->philo[i].right = i + 1;
+		if (i == (g->args->philo_nbr - 1))
+		{
+			g->philo[i].left = 0;
+			g->philo[i].right = i;
+		}
+		g->philo[i].lastmeal_ms = 0;
+		g->philo[i].meals = 0;
+	}
+	return (0);
+}
+
+int	alloc_all(t_global *g)
+{
+	int	i;
+
+	g->mutexes = malloc(sizeof(pthread_mutex_t) * g->args->philo_nbr);
+	g->print = malloc(sizeof(pthread_mutex_t));
+	g->deathmtx = malloc(sizeof(pthread_mutex_t));
+	g->arrmtx = malloc(sizeof(pthread_mutex_t));
+	g->philo = malloc(sizeof(t_philo) * g->args->philo_nbr);
+	g->arr = malloc(sizeof(int) * (g->args->philo_nbr + 1));
+	if (!g->mutexes || !g->print || !g->deathmtx || !g->arrmtx
+		|| !g->philo || !g->arr)
+		return (1);
+	i = -1;
+	while (++i < g->args->philo_nbr + 1)
+		g->arr[i] = 0;
+	i = -1;
+	while (++i < g->args->philo_nbr)
+		pthread_mutex_init(&(g->mutexes[i]), NULL);
+	pthread_mutex_init(g->print, NULL);
+	pthread_mutex_init(g->deathmtx, NULL);
+	pthread_mutex_init(g->arrmtx, NULL);
+	init_philo(g);
+	return (0);
+}
+
+int	parse_args(t_global *g, int ac, char **av)
+{
+	g->args->init_ms = 0;
+	g->args->death_track = 0;
+	g->args->philo_nbr = ft_atoi(av[1]);
+	g->args->starve_time = ft_atoi(av[2]);
+	g->args->eat_dur = ft_atoi(av[3]);
+	g->args->sleep_dur = ft_atoi(av[4]);
+	g->args->nbr_of_meals = -1;
+	if (ac == 6)
+	{
+		g->args->nbr_of_meals = ft_atoi(av[5]);
+	}	
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_global	g;
+
+	if (ac < 5 || ac > 6)
+		return (1);
+	g.args = malloc(sizeof(t_args));
+	if (!g.args)
+		return (1);
+	if (parse_args(&g, ac, av))
+		return (1);
+	if (alloc_all(&g))
+		return (1);
+	ft_simulation(g.philo);
+	free_all(&g);
+}
---
+++ philo/philo_utils.c
@@ -0,0 +1,49 @@
+#include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	i = 0;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	sign = 1;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ((str[i] >= '0' && str[i] <= '9') && str[i] != '\0')
+	{
+		result = (result * 10) + str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+void	ft_print_msg(t_philo *philo, char *str)
+{
+	struct timeval			t;
+	unsigned long			ms;
+
+	gettimeofday(&t, NULL);
+	ms = (t.tv_sec * 1000) + (t.tv_usec / 1000);
+	pthread_mutex_lock(philo->printmtx);
+	printf("[%lu ms] %d %s\n", ms - philo->args->init_ms, philo->id, str);
+	pthread_mutex_unlock(philo->printmtx);
+}
+
+void	free_all(t_global *g)
+{
+	free(g->mutexes);
+	free(g->arr);
+	free(g->args);
+	free(g->print);
+	free(g->deathmtx);
+	free(g->arrmtx);
+	free(g->philo);
+}
